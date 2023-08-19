46ebdabc1f6e3d35426967f65a2c6e647f221b82
luca augang <lucaaufgang06@gmail.com>
2023-08-19 12:49:04 -0300

    trabajolucaaufgang

--- index.js
+++ index.js
@@ -4,36 +4,35 @@ const inputCuotas = document.getElementById("cuotas")
 const resp = document.getElementById("respuesta")
 
 const boton = document.getElementById("Boton")
-
+boton.addEventListener("click", mostrarResultado)
 
 function mostrarResultado(){
     const mostrar = inputMonto.value / inputCuotas.value
-    resp.innertext = mostrar
+    resp.innerText = mostrar
 }
-boton.addEventListener("click", mostrarResultado)
 const miArray = [1,3,6,12,18,24]
 
-if (segundoNumero != miArray[0]){
-    let respuesta = ingresarNumero / miArray[0]
+if (resp != miArray[0]){
+    let result = inputCuotas / miArray[0]
     console.log("pagando en 1 cuotas, las cuotas son de $" + respuesta );
 }
-else if(segundoNumero != miArray[1]){
-    let respuesta2 = ingresarNumero / miArray[1]
+else if(resp != miArray[1]){
+    let result2 = inputCuotas / miArray[1]
     console.log("pagando en 3 cuotas, las cuotas son de $" + respuesta2 );
 }
-else if(segundoNumero != miArray[2]){
-    let respuesta3 = ingresarNumero / miArray[2]
+else if(resp != miArray[2]){
+    let rresult3 = inputCuotas / miArray[2]
     console.log("pagando en 6 cuotas, las cuotas son de $" + respuesta3 );
 }
-else if(segundoNumero != miArray[3]){
-    let respuesta4 = ingresarNumero / miArray[3]
+else if(resp != miArray[3]){
+    let result4 = inputCuotas / miArray[3]
     console.log("pagando en 12 cuotas, las cuotas son de $" + respuesta4 );
 }
-else if(segundoNumero != miArray[4]){
-    let respuesta5 = ingresarNumero / miArray[4]
+else if(resp != miArray[4]){
+    let result5 = inputCuotas / miArray[4]
     console.log("pagando en 18 cuotas, las cuotas son de $" + respuesta5 );
 }
-else if(segundoNumero != miArray[5]) {
-    let respuesta6 = ingresarNumero / miArray[5]
+else if(resp != miArray[5]) {
+    let result6 = inputCuotas / miArray[5]
     console.log("pagando en 24 cuotas, las cuotas son de $" + respuesta6 );
 }
