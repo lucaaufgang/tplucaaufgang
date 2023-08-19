ca7bcd2ae05447869ef7e0b183b040b1fdedea69
luca augang <lucaaufgang06@gmail.com>
2023-08-19 12:51:12 -0300

    trabajolucaaufgang

--- index.js
+++ index.js
@@ -13,26 +13,26 @@ function mostrarResultado(){
 const miArray = [1,3,6,12,18,24]
 
 if (resp != miArray[0]){
-    let result = inputCuotas / miArray[0]
-    console.log("pagando en 1 cuotas, las cuotas son de $" + respuesta );
+    let result1 = inputCuotas / miArray[0]
+    console.log("pagando en 1 cuotas, las cuotas son de $" + result1 );
 }
 else if(resp != miArray[1]){
     let result2 = inputCuotas / miArray[1]
-    console.log("pagando en 3 cuotas, las cuotas son de $" + respuesta2 );
+    console.log("pagando en 3 cuotas, las cuotas son de $" + result2 );
 }
 else if(resp != miArray[2]){
     let rresult3 = inputCuotas / miArray[2]
-    console.log("pagando en 6 cuotas, las cuotas son de $" + respuesta3 );
+    console.log("pagando en 6 cuotas, las cuotas son de $" + result3 );
 }
 else if(resp != miArray[3]){
     let result4 = inputCuotas / miArray[3]
-    console.log("pagando en 12 cuotas, las cuotas son de $" + respuesta4 );
+    console.log("pagando en 12 cuotas, las cuotas son de $" + result4 );
 }
 else if(resp != miArray[4]){
     let result5 = inputCuotas / miArray[4]
-    console.log("pagando en 18 cuotas, las cuotas son de $" + respuesta5 );
+    console.log("pagando en 18 cuotas, las cuotas son de $" + result5 );
 }
 else if(resp != miArray[5]) {
     let result6 = inputCuotas / miArray[5]
-    console.log("pagando en 24 cuotas, las cuotas son de $" + respuesta6 );
+    console.log("pagando en 24 cuotas, las cuotas son de $" + result6 );
 }
