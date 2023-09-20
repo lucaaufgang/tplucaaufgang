08eea386a6ec2b22f1fde66c169831fcf26f0ec8
luca augang <lucaaufgang06@gmail.com>
2023-09-20 16:50:40 -0300

    first commit

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function calcularPrestamo() {
         const respuesta = monto * CuotaPrestamo
         const respuesta1 =  monto * interes / 12
         const respuestaFinal = respuesta + respuesta1
-        resp2.innerText = nombre + 'debe pagar' + respuestaFinal
+        resp2.innerText = nombre + ' debe pagar ' + respuestaFinal
     }
    
 
