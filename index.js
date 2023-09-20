e653e2f39408c9ba7b9066a51f13e2011c7b7a7c
luca augang <lucaaufgang06@gmail.com>
2023-09-20 16:37:42 -0300

    first commit

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ const inputCuotas = document.getElementById("cuotas");
 const resp = document.getElementById("respuesta");
 const boton1 = document.getElementById("boton1");
 
-const cuotasArray = ["1","3","6","12","18","24"]
+const cuotasArray = ["1","3","6","12","18","24", "30", "32", "36", "48"]
 
 boton1.addEventListener("click", mostrarResultado);
 
 
 function mostrarResultado() {
     const numCuotas = inputCuotas.value 
-
-        
-        if (inputMonto.value === '') {
+            if (inputMonto.value === '') {
             alert('Por favor, indicar el monto');
             resp.innerText = "debe indicar los valores"
-            return;
         }
-       
         if (cuotasArray.includes(numCuotas)) {
             calcularCuotas(inputMonto.value, parseInt(numCuotas));
         } else {
@@ -25,6 +21,10 @@ function mostrarResultado() {
                 }
     }
 
+    function calcularCuotas(inputMonto, cuotas){
+        const result = inputMonto / cuotas;
+        resp.innerText = 'cada cuota es de $' + result;
+    }
 
     const inputNombre = document.getElementById("nombre");
 const inputMontoPrestamo = document.getElementById("montoPrestamo");
@@ -35,21 +35,34 @@ const boton2 = document.getElementById("boton2");
 boton2.addEventListener("click", calcularPrestamo);
 
 function calcularPrestamo() {
+    const numeroCuotas = inputCuotasPrestamo.value
     const prestamo ={
-        nombre: inputNombre,
-        monto: inputMontoPrestamo,
-        Cuotaprestamo: inputCuotasPrestamo,
-        interes: inputInteres,
+        nombre: inputNombre.value,
+        monto: inputMontoPrestamo.value,
+        Cuotaprestamo: numeroCuotas.value,
+        interes: inputInteres.value,
     }
-        const respuesta = monto * Cuotaprestamo
+
+    if (cuotasArray.includes(numeroCuotas)) {
+        FinalPrestamo(inputMontoPrestamo,inputInteres,parseInt(numeroCuotas));
+    } else {
+        console.log('Número de cuotas no válido.');
+            }
+        const respuesta = monto * numeroCuotas
         const respuesta1 =  monto * interes / 12
         const respuestaFinal = respuesta + respuesta1
+       
         if (prestamo === '') {
             alert('Por favor, indicar los valores');
             resp.innerText = "debe indicar los valores"
         }
     }
-
+    function FinalPrestamo(inputMontoPrestamo,inputInteres, cuotasPrestamo,inputNombre){
+        const respuesta = inputMontoPrestamo * cuotasPrestamo
+        const respuesta1 =  inputMontoPrestamo * inputInteres / 12
+        const respuestaFinal = respuesta + respuesta1
+        resp2.innerText = inputNombre + 'debe pagar' + respuestaFinal
+    }
    
 
 
